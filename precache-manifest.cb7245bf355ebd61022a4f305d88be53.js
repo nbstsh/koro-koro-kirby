86f2b66975a9176fdd966e999d98f9755a47413e
nbstsh <nbstsh@gmail.com>
2019-08-10 02:42:58 +0900

    Updates

---
+++ precache-manifest.cb7245bf355ebd61022a4f305d88be53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bbb43045ccee62f87d0011617cc7960",
+    "url": "/koro-koro-kirby/index.html"
+  },
+  {
+    "revision": "ebb2aa9b430880b6a766",
+    "url": "/koro-koro-kirby/static/css/main.1971b7cc.chunk.css"
+  },
+  {
+    "revision": "2c39eba7ff71ace3f5aa",
+    "url": "/koro-koro-kirby/static/js/2.0122a371.chunk.js"
+  },
+  {
+    "revision": "ebb2aa9b430880b6a766",
+    "url": "/koro-koro-kirby/static/js/main.f68decef.chunk.js"
+  },
+  {
+    "revision": "d89f34488c34664aaac7",
+    "url": "/koro-koro-kirby/static/js/runtime~main.cc406e74.js"
+  }
+]);
